c89c6a2bd084c3e2b7a40620bba482a56f842957
DavidLineman <dline215@gmail.com>
2019-03-15 15:56:31 -0400

    fixed the results partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,20 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
 
 
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    return false unless stock
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  def under_stock_limit?
+    (user_stocks.count < 10)
+  end
+
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"  
